155d54001ff7e89a2feea32c7c375b953b2f111b
ArvoX <github@arvox.dk>
2010-08-11 17:18:27 +0200

    now with caching of cache

--- MarkovWord.py
+++ MarkovWord.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python
 import random
+import os
 
 class Markov:
-    def __init__(self, file, size):
-        self.size = size
+    def __init__(self):
+        self.size = 3 
         self.starts = []
         self.cache = {}
-        self.file_to_words(file)
-        self.parse_words()
+        if(os.path.getmtime('names.txt') > os.path.getmtime('cache.file')):
+	    file = open('names.txt')
+	    self.file_to_words(file)
+            self.parse_words()
+	    cachefile = open('cache.file', 'w')
+	    cachefile.write(repr(self.cache))
+	    cachefile.close()
+	    self.file.close()
+	else:
+	    cachefile = open('cache.file')
+	    self.cache = eval(cachefile.read())
+	    cachefile.close()
+	    self.starts = self.cache.keys()
 
     def file_to_words(self, file):
         file.seek(0)
@@ -43,22 +55,9 @@ class Markov:
             next = random.choice(self.cache[key])
         return word
 
-from optparse import OptionParser
-
 def main():
-    parser = OptionParser()
-    parser.add_option('-p', type='int', dest='prev_num', default=3,
-                      help='number of previous letters to base chain on')
-    parser.add_option('-n', type='int', dest='num', default=5,
-                      help='number of generated words')
-    parser.add_option('-s', '--source-text', type='string',
-                      default='wordlist-en.txt', dest='source',
-                      help='file to use as basis for generating the words')
-    (options, args) = parser.parse_args(['-p','3','-n','10','-s','names.txt'])
-
-    file = open(options.source)
-    markov = Markov(file, options.prev_num)
-    for i in range(options.num):
+    markov = Markov()
+    for i in range(10):
         print markov.generate_word().capitalize()
 
 if __name__ == '__main__':
